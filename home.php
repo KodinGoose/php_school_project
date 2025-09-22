eb37cd05fad836bc621f9991b37bd965e6a178bb
KodinGoose <baronovicsbarnabas@gmail.com>
2025-09-22 08:00:27 +0200

    Create home page

--- home.php
+++ home.php
@@ -1,56 +1,25 @@
-<table id = "main_table">
-<tr>
-    <td id = "reg">
-    <form action = "register.php" method = "GET">
-    <table id = "reg_table">
-        <tr>
-            <td colspan = "2">Regisztráció</td>
-        </tr>
-        <tr>
-            <td class = "info_col">Név: </td>
-            <td><input name = "reg_name"></td>
-        </tr>
-        <tr>
-            <td class = "info_col">Jelszó: </td>
-            <td><input name = "reg_pass" type = "password"></td>
-        </tr>
-        <tr>
-            <td class = "info_col">Születési év: </td>
-            <td><input name = "reg_szulev" type = "date"></td>
-        </tr>
-        <tr>
-            <td class = "info_col">Telefonszám: </td>
-            <td><input name = "reg_phone" type = "tel"></td>
-        </tr>
-        <tr>
-            <td class = "info_col">Email: </td>
-            <td><input name = "reg_email" type = "email"></td>
-        </tr>
-        <tr class = "submit">
-            <td colspan = "2"><input name = "register" type = "submit" value = "Regisztálás"></td>
-        </tr>
-    </table>
-    </form>
-    </td>
-    <td id = "sign">
-    <form action = "sign_in.php" method = "GET">
-    <table id = "sign_table">
-        <tr>
-            <td colspan = "2">Bejelentkezés</td>
-        </tr>
-        <tr>
-            <td class = "info_col">Név: </td>
-            <td><input name = "sign_name"></td>
-        </tr>
-        <tr>
-            <td class = "info_col">Jelszó: </td>
-            <td><input name = "sign_pass" type = "password"></td>
-        </tr>
-        <tr class = "submit">
-            <td colspan = "2"><input name = "sign_in" type = "submit" value = "Bejelentkezés"></td>
-        </tr>
-    </table>
-    </form>
-    </td>
-</tr>
+<table id = "data_table">
+    <tr>
+        <td class = "info_col">Név: </td>
+        <td><?php print $_SESSION["name"] ?></td>
+    </tr>
+    <tr>
+        <td class = "info_col">Jelszó: </td>
+        <td><?php print $_SESSION["pass"] ?></td>
+    </tr>
+    <tr>
+        <td class = "info_col">Születési év: </td>
+        <td><?php print $_SESSION["szulev"] ?></td>
+    </tr>
+    <tr>
+        <td class = "info_col">Telefonszám: </td>
+        <td><?php print $_SESSION["phone"] ?></td>
+    </tr>
+    <tr>
+        <td class = "info_col">Email: </td>
+        <td><?php print $_SESSION["email"] ?></td>
+    </tr>
 </table>
+<form id = "logout_form" action = "index.php" method = "GET">
+    <input id = "logout_button" name = "logout" value = "Kilépés" type = "submit">
+</from>
